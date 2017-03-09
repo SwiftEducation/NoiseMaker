8f6124ca4d5f0bd0ddfe61848183b611d20f4caa
Gabe McFarlane <gabrielmcfarlane65@gmail.com>
2017-03-09 13:34:01 -0800

    Update for Xcode 8 and Swift 3. References #9
    Signed-off-by: Gabe McFarlane <gabrielmcfarlane65@gmail.com>
    Reviewed-by: Yong Bakos <ybakos@humanoriented.com>

--- NoiseMaker/ViewController.swift
+++ NoiseMaker/ViewController.swift
@@ -25,30 +25,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func playGuitar(sender: UIButton) {
-        if let url = NSBundle.mainBundle().URLForResource("guitar", withExtension: "wav") {
-            guitarPlayer = try? AVAudioPlayer(contentsOfURL: url)
+    @IBAction func playGuitar(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "guitar", withExtension: "wav") {
+            guitarPlayer = try? AVAudioPlayer(contentsOf: url)
             guitarPlayer?.play()
         }
     }
     
-    @IBAction func playApplause(sender: UIButton) {
-        if let url = NSBundle.mainBundle().URLForResource("applause", withExtension: "wav") {
-            applausePlayer = try? AVAudioPlayer(contentsOfURL: url)
+    @IBAction func playApplause(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "applause", withExtension: "wav") {
+            applausePlayer = try? AVAudioPlayer(contentsOf: url)
             applausePlayer?.play()
         }
     }
     
-    @IBAction func playMonster(sender: UIButton) {
-        if let url = NSBundle.mainBundle().URLForResource("monster", withExtension: "wav") {
-            monsterPlayer = try? AVAudioPlayer(contentsOfURL: url)
+    @IBAction func playMonster(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "monster", withExtension: "wav") {
+            monsterPlayer = try? AVAudioPlayer(contentsOf: url)
             monsterPlayer?.play()
         }
     }
     
-    @IBAction func playBubbles(sender: UIButton) {
-        if let url = NSBundle.mainBundle().URLForResource("bubbles", withExtension: "wav") {
-            bubblesPlayer = try? AVAudioPlayer(contentsOfURL: url)
+    @IBAction func playBubbles(_ sender: UIButton) {
+        if let url = Bundle.main.url(forResource: "bubbles", withExtension: "wav") {
+            bubblesPlayer = try? AVAudioPlayer(contentsOf: url)
             bubblesPlayer?.play()
         }
     }
